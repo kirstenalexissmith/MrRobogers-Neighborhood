2fb283124600b88c3b62191c2b1338c1022fa379
Kirsten Smith <kirstenalexis7@gmail.com>
2023-11-19 19:02:02 -0800

    add subNumber function

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,20 @@ window.addEventListener("load", function () {
 
 function subNumber(e) {
   e.preventDefault();
+  const numberInput = document.getElementById('numberInput').value;
+  const resultList = [];
 
+  for (let i = 0; i <= numberInput; i++) {
+    let currentNumber = i.toString();
+
+    if (currentNumber.includes('1')) {
+      resultList.push("Beep!");
+    } else if (currentNumber.includes('2')) {
+      resultList.push("Boop!");
+    } else if (currentNumber.includes('3')) {
+      resultList.push("Won't you be my neighbor?");
+    } else {
+      resultList.push(currentNumber);
+    }
+  }
 }
